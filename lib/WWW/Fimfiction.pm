563124393d51d921b63bebb078e1cfe49e0b7a71
Cameron Thornton <cthor@cpan.org>
2013-01-19 12:23:12 +0000

    Import of CTHOR/WWW-Fimfiction-v0.3.0 from CPAN.
    gitpan-cpan-distribution: WWW-Fimfiction
    gitpan-cpan-version:      v0.3.0
    gitpan-cpan-path:         CTHOR/WWW-Fimfiction-v0.3.0.tar.gz
    gitpan-cpan-author:       CTHOR
    gitpan-cpan-maturity:     released

--- lib/WWW/Fimfiction.pm
+++ lib/WWW/Fimfiction.pm
@@ -1,96 +1,262 @@
 package WWW::Fimfiction;
 
-use 5.006;
+use 5.014;
 use strict;
 use warnings FATAL => 'all';
-use base 'Exporter';
+use HTML::TreeBuilder;
 use LWP::UserAgent;
+use HTTP::Cookies;
+use XML::Twig;
 use Carp 'croak';
 use JSON 'decode_json';
-use WWW::Fimfiction::_types;
-use WWW::Fimfiction::Story;
-use WWW::Fimfiction::User;
-use WWW::Fimfiction::Chapter;
 
-our @EXPORT_OK = ('story');
-our $VERSION = 'v0.2.1';
-
-my $ua = LWP::UserAgent->new( timeout => 10 );
-$ua->agent(__PACKAGE__ . ' ');
+our $VERSION = 'v0.3.0';
 
 =head1 NAME
 
-WWW::Fimfiction - Data from fimfiction.net
-
-=head1 SYNOPSIS
-
-  use WWW::Fimfiction qw/story/;
-
-  my $story = story(6762);
-  print $story->title; # 'To Catch a Stallion'
-
-=head1 FUNCTIONS
-
-=head2 story
-
-Returns a L<WWW::Fimfiction::Story> object from a story ID. Croaks on error.
+WWW::Fimfiction - CRUD tasks for fimfiction.net
 
 =cut
 
-sub story {
-	my $id = shift;
+=head1 SYNOPSIS
 
-	my $res = $ua->get("http://www.fimfiction.net/api/story.php?story=$id");
+	use WWW::Fimfiction;
+
+	my $ua = WWW::Fimfiction->new;
+
+	$ua->login($username, $password);
+
+	$ua->add_chapter($story_id, 'My Fabulous Chapter %i%', $text);
+
+=head1 METHODS
+
+Methods without explicit return values will return the WWW::Fimfiction object. Methods
+will croak if something goes wrong.
+
+=head2 new
+
+Makes a new object.
+
+=cut
+
+sub new {
+	my $class = shift;
+
+	my $ua = LWP::UserAgent->new( cookie_jar => HTTP::Cookies->new );
+	$ua->agent("WWW-Fimfiction/$VERSION ");
+
+	return bless { ua => $ua }, $class;
+}
+
+sub _ua {
+	my $self = shift;
+	return $self->{ua};
+}
+
+sub _assert_auth {
+	my $self = shift;
+	unless( $self->{auth} ) {
+		croak "Authentication required. Try calling ->login first.";
+	}
+}
+
+sub _post {
+	my $self = shift;
+
+	my $res = $self->_ua->post(@_);
 
 	if( $res->is_success ) {
-		my $data = decode_json $res->decoded_content;
-
-		if( defined $data->{error} ) {
-			croak 'Error: ' . $data->{error};
-		}
-
-		my $story = $data->{story};
-		my $tags = $story->{categories};
-		my $chapters = $story->{chapters};
-
-		return WWW::Fimfiction::Story->new(
-			author => WWW::Fimfiction::User->new(
-				id   => $story->{author}{id},
-				name => $story->{author}{name},
-			),
-			id          => $story->{id},
-			title       => $story->{title},
-			description => $story->{description},
-			rating      => $story->{content_rating_text},
-			tags        => [ grep { $tags->{$_} } keys %$tags ],
-			wordcount   => $story->{words},
-			status      => $story->{status},
-			uri         => $story->{url},
-			image_uri   => $story->{image},
-			views       => $story->{views},
-			chapters    => [ map {
-				WWW::Fimfiction::Chapter->new(
-					id        => $_->{id},
-					title     => $_->{title},
-					wordcount => $_->{words},
-					uri       => $_->{link},
-					views     => $_->{views},
-					updated   => $_->{date_modified},
-				)
-			} @$chapters ],
-			updated => $story->{date_modified},
-		);
+		return $res;
 	}
 	else {
 		croak "Error: " . $res->status_line;
 	}
 }
 
-=head1 SEE ALSO
+sub _get {
+	my $self = shift;
 
-L<WWW::Fimfiction::Story>,
-L<WWW::Fimfiction::User>,
-L<WWW::Fimfiction::Chapter>
+	my $res = $self->_ua->get(@_);
+
+	if( $res->is_success ) {
+		return $res;
+	}
+	else {
+		croak "Error: " . $res->status_line;
+	}
+}
+
+=head2 login
+
+Args: ($username, $password)
+
+Authenticates the user. Tasks that manipulate data on the site require authentication,
+so you'll have to call this before trying to add/edit/delete stuff.
+
+=cut
+
+sub login {
+	my( $self, $username, $password ) = @_;
+
+	my $res = $self->_post('http://www.fimfiction.net/ajax/login.php', {
+		username => $username,
+		password => $password,
+	});
+
+	my $code = $res->decoded_content;
+
+	if( $code eq '0' ) {
+		$self->{auth} = $username;
+		return $self;
+	}
+	elsif( $code eq '1' ) {
+		croak 'Invalid password';
+	}
+	elsif( $code eq '2' ) {
+		croak 'Invalid username';
+	}
+	else {
+		croak 'Error: $code';
+	}
+}
+
+=head2 add_chapter
+
+Args: ($story_id, [$chapter_title, $content])
+
+Adds a chapter to the given story. Returns the chapter id.
+
+If provided, additional arguments will be given to edit_chapter().
+
+=cut
+
+sub add_chapter {
+	my( $self, $story_id, $chapter_title, $content ) = @_;
+	my $chapter_id;
+
+	$self->_assert_auth;
+
+	my $form = { story => $story_id, title => $chapter_title };
+
+	my $res = $self->_post('http://www.fimfiction.net/ajax/modify_chapter.php', $form);
+
+	my $elt = XML::Twig::Elt->parse($res->decoded_content);
+
+	if( my $error = $elt->field('error') ) {
+		croak $error;
+	}
+
+	unless( $chapter_id = $elt->field('id') ) {
+		croak "Unexpected response: " . $res->decoded_content;
+	}
+
+	if( defined $content ) {
+		$self->edit_chapter($chapter_id, $chapter_title, $content);
+	}
+
+	return $chapter_id;
+}
+
+=head2 edit_chapter
+
+Args: ($id, $title, $content)
+
+Edits chapter with the given title and content.
+
+=cut
+
+sub edit_chapter {
+	my( $self, $id, $title, $content ) = @_;
+
+	$self->_assert_auth;
+
+	my $form = { chapter => $id, title => $title, content => $content };
+
+	my $res = $self->_post('http://www.fimfiction.net/ajax/modify_chapter.php', $form);
+
+	my $elt = XML::Twig::Elt->parse($res->decoded_content);
+
+	if( my $error = $elt->field('error') ) {
+		croak $error;
+	}
+	else {
+		return $self;
+	}
+}
+
+=head2 publish_chapter
+
+Args: ($id)
+
+Toggles the publish status of a chapter. Returns 1 or 0 indicating the chapter's publish status.
+
+=cut
+
+sub publish_chapter {
+	my( $self, $id ) = @_;
+
+	$self->_assert_auth;
+
+	my $form = { chapter => $id };
+
+	my $res = $self->_post('http://www.fimfiction.net/ajax/publish_chapter.php', $form);
+
+	my $elt = XML::Twig::Elt->parse($res->decoded_content);
+
+	if( my $error = $elt->field('error') ) {
+		croak $error;
+	}
+	elsif( ( my $status = $elt->field('published') ) ne '' ) {
+		return $status;
+	}
+	else {
+		croak "Unexpected response: " . $res->decoded_content;
+	}
+}
+
+=head2 delete_chapter
+
+Args: ($id)
+
+Deletes a chapter.
+
+=cut
+
+sub delete_chapter {
+	my ( $self, $id ) = @_;
+
+	$self->_assert_auth;
+
+	my $form = { chapter => $id, confirm => 'on' };
+
+	# Get the form first, which has a 'noonce' value to confirm deletion (why?)
+	my $res = $self->_get("http://www.fimfiction.net/?view=delete_chapter&chapter=$id");
+
+	my $tree = HTML::TreeBuilder->new;
+	$tree->parse_content($res->decoded_content);
+
+	$form->{noonce} = $tree->look_down(_tag => 'input', name => 'noonce')->attr('value')
+		or croak "Unable to find hidden 'noonce' input field";
+
+	# Do the actual deletion
+	$self->_post('http://www.fimfiction.net/index.php?view=delete_chapter', $form);
+
+	return $self;
+}
+
+=head2 get_story
+
+Returns a hash ref of story metadata.
+
+=cut
+
+sub get_story {
+	my( $self, $id ) = @_;
+
+	my $res = $self->_get("http://www.fimfiction.net/api/story.php?story=$id");
+
+	return decode_json($res->decoded_content)->{story};
+}
 
 =head1 AUTHOR
 
--- t/01-story.t
+++ t/01-story.t
@@ -3,109 +3,26 @@ use 5.006;
 use strict;
 use warnings FATAL => 'all';
 use Test::More;
-use WWW::Fimfiction 'story';
+use WWW::Fimfiction;
 
-my $story = eval{ story(6762) };
+my $ua = WWW::Fimfiction->new;
+
+my $story = eval{ $ua->get_story(6762) };
 
 if($@ =~ /^Error: 503 Service Unavailable/) {
 	plan skip_all => 'Fimfiction service currently unavailable';
 }
 
 is(
-	$story->id, 
+	$story->{id}, 
 	6762,
 	'ID as expected',
 );
 
 is(
-	$story->title, 
+	$story->{title}, 
 	'To Catch a Stallion',
 	'Title as expected',
-);
-
-isa_ok(
-	$story->author,
-	'WWW::Fimfiction::User',
-	'Author',
-);
-
-is(
-	$story->author->name, 
-	'RogerDodger',
-	'Author name as expected',
-);
-
-is(
-	$story->image_uri->host,
-	'www.fimfiction-static.net',
-	'Image host as expected',
-);
-
-is(
-	$story->rating,
-	'Teen',
-	'Rating as expected',
-);
-
-is(
-	$story->status,
-	'Complete',
-	'Status as expected',
-);
-
-ok(
-	$story->wordcount < 1000,
-	'Wordcount as expected',
-);
-
-is(
-	substr($story->description, 0, 10),
-	'Some ponie',
-	'Description as expected',
-);
-
-is_deeply(
-	$story->tags,
-	[ 'Comedy' ],
-	'Tags as expected',
-);
-
-is(
-	substr($story->uri->path, 0, 12),
-	'/story/6762/',
-	'URI path as expected',
-);
-
-isa_ok(
-	$story->updated,
-	'DateTime',
-	'Story mtime',
-);
-
-isa_ok(
-	$story->chapters->[0],
-	'WWW::Fimfiction::Chapter',
-	'Chapter 1',
-);
-
-isa_ok(
-	$story->chapters->[0]->updated,
-	'DateTime',
-	'Chapter 1 mtime'
-);
-
-$story = story(66145);
-
-is(
-	$story->image_uri,
-	undef,
-	'No image, as expected'
-);
-
-is_deeply(
-	$story->tags,
-	[],
-	'No tags, as expected'
 );
 
 done_testing;
